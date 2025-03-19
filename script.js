c62ce904ce3bb0e9d20747087f9569e4d0f05179
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2025-03-19 02:26:23 -0400

    sfs

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const sandwishes = [
   { nombre: "Pan con Lechón-------------------", precio: "400.00" },
-  { nombre: "Pan con Jamón ", precio: "300.00" },
-  { nombre: "Pan con Perro ", precio: "225.00" },
-  { nombre: "Pan/Jamón Jamón", precio: "600.00" },
-  { nombre: "Pan/Jamón y Queso", precio: "700.00" },
-  { nombre: "Pan con Lechón WAOO!", precio: "800.00" }
+  { nombre: "Pan con Jamón--------------------", precio: "300.00" },
+  { nombre: "Pan con Perro--------------------", precio: "225.00" },
+  { nombre: "Pan/Jamón Jamón------------------", precio: "600.00" },
+  { nombre: "Pan/Jamón y Queso----------------", precio: "700.00" },
+  { nombre: "Pan con Lechón WAOO!-------------", precio: "800.00" }
 ];
 
 const bebidas = [
   { nombre: "Jugos de Caja--------------------", precio: "190.00" },
-  { nombre: "Refrescos de Lata", precio: "220.00" },
-  { nombre: "Cervezas", precio: "250.00" },
-  { nombre: "Cerveza Cristal", precio: "300.00" },
-  { nombre: "Malteada", precio: "500.00" },
-  { nombre: "Batidos de Helado", precio: "800.00" },
-  { nombre: "Michelada (Cerveza Cristal)", precio: "600.00" },
-  { nombre: "Michelada (Cacique)", precio: "500.00" }
+  { nombre: "Refrescos de Lata----------------", precio: "220.00" },
+  { nombre: "Cervezas-------------------------", precio: "250.00" },
+  { nombre: "Cerveza Cristal------------------", precio: "300.00" },
+  { nombre: "Malteada-------------------------", precio: "500.00" },
+  { nombre: "Batidos de Helado----------------", precio: "800.00" },
+  { nombre: "Michelada (Cerveza Cristal)------", precio: "600.00" },
+  { nombre: "Michelada (Cacique)--------------", precio: "500.00" }
 ];
 
 const entrantes = [
-  { nombre: "Fritas de Malanga", precio: "400.00" },
-  { nombre: "Croquetas", precio: "450.00" },
-  { nombre: "Tostones Rellenos(J/Queso)", precio: "700.00" },
-  { nombre: "Frituras de Malanga", precio: "700.00" },
-  { nombre: "Picadera Waoo!", precio: "2000.00" }
+  { nombre: "Fritas de Malanga----------------", precio: "400.00" },
+  { nombre: "Croquetas------------------------", precio: "450.00" },
+  { nombre: "Tostones Rellenos(J/Queso)-------", precio: "700.00" },
+  { nombre: "Frituras de Malanga--------------", precio: "700.00" },
+  { nombre: "Picadera Waoo!------------------", precio: "2000.00" }
 ];
 
 const especialidad = [
-  { nombre: "Pollo Chiflado", precio: "1000.00" },
-  { nombre: "Crudo de Pescado", precio: "450.00" },
-  { nombre: "Ensalada Fría", precio: "300.00" },
-  { nombre: "El Wapeton (helado, dulces, nutella y confituras)------------------------", precio: "1500.00" },
-  { nombre: "Pan con Perro Perro", precio: "450.00" }
+  { nombre: "Pollo Chiflado------------------", precio: "1000.00" },
+  { nombre: "Crudo de Pescado-----------------", precio: "450.00" },
+  { nombre: "Ensalada Fría--------------------", precio: "300.00" },
+  { nombre: "El Wapeton----------------------", precio: "1500.00" },
+  { nombre: "Pan con Perro Perro--------------", precio: "450.00" }
 ];
 
 // Mostrar los platos en la página
