966b008e6b9bda16bdff83d894b69672672c4834
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2025-03-19 02:33:31 -0400

    qwe

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const sandwishes = [
   { nombre: "Pan con Lechón-------------------", precio: "400.00" },
-  { nombre: "Pan con Jamón--------------------", precio: "300.00" },
+  { nombre: "Pan con Jamón------------------", precio: "300.00" },
   { nombre: "Pan con Perro----------------------", precio: "225.00" },
   { nombre: "Pan/Jamón Jamón------------------", precio: "600.00" },
-  { nombre: "Pan/Jamón y Queso----------------", precio: "700.00" },
-  { nombre: "Pan con Lechón WAOO!-----------", precio: "800.00" }
+  { nombre: "Pan/Jamón y Queso---------------", precio: "700.00" },
+  { nombre: "Pan con Lechón WAOO!------------", precio: "800.00" }
 ];
 
 const bebidas = [
@@ -19,19 +19,19 @@ const bebidas = [
 ];
 
 const entrantes = [
-  { nombre: "Fritas de Malanga------------------", precio: "400.00" },
+  { nombre: "Fritas de Malanga-----------------", precio: "400.00" },
   { nombre: "Croquetas--------------------------", precio: "450.00" },
-  { nombre: "Tostones Rellenos(J/Queso)-------", precio: "700.00" },
+  { nombre: "Tostones Rellenos(J/Queso)-----", precio: "700.00" },
   { nombre: "Frituras de Malanga---------------", precio: "700.00" },
-  { nombre: "Picadera Waoo!-------------------", precio: "2000.00" }
+  { nombre: "Picadera Waoo!------------------", precio: "2000.00" }
 ];
 
 const especialidad = [
-  { nombre: "Pollo Chiflado------------------", precio: "1000.00" },
-  { nombre: "Crudo de Pescado-----------------", precio: "450.00" },
-  { nombre: "Ensalada Fría--------------------", precio: "300.00" },
+  { nombre: "Pollo Chiflado--------------------", precio: "1000.00" },
+  { nombre: "Crudo de Pescado----------------", precio: "450.00" },
+  { nombre: "Ensalada Fría---------------------", precio: "300.00" },
   { nombre: "El Wapeton----------------------", precio: "1500.00" },
-  { nombre: "Pan con Perro Perro--------------", precio: "450.00" }
+  { nombre: "Pan con Perro Perro-------------", precio: "450.00" }
 ];
 
 // Mostrar los platos en la página
